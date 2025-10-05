251a072e25b2b1199c879916c2a53d15bfcae30e
AlexNaziru <alex.naziru.dev@gmail.com>
2025-10-05 22:05:19 +0300

    render rdy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+    build: {
+        outDir: "dist"
+    },
+    server: {
+        port: 5173
+    },
+    preview: {
+        port: 10000
+    }
 })
